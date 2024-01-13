1fd0a235a043a6d4aabe2477fc341ead64bc320b
Aaron <aaron-dario.fries@fh-campus.ac.at>
2024-01-13 23:37:19 +0100

    expand Map

--- Editor/lib/circle.py
+++ Editor/lib/circle.py
@@ -1,15 +1,17 @@
+import json
+
 class c_circle:
 
     circles = {}
     next_id = 1
 
-    def __init__(self, x, y, node_type, floor, cooridinates_absolute, name = "") -> None:
+    def __init__(self, x, y, node_type, floor, cooridinates_absolute, name = "", connections = None) -> None:
 
         self.x = x
         self.y = y
         self.cooridinates_absolute = cooridinates_absolute
         self.node_type = node_type
-        self.connections = []
+        self.connections = connections if connections != None else []
         self.floor = floor
         self.name = name
         self.id = c_circle.next_id
@@ -17,8 +19,9 @@ class c_circle:
         c_circle.circles[c_circle.next_id] = self
 
     def delet(self):
-        for circle in self.connections:
-            c_circle.circles[circle].connections.remove(self.id)
+        for circle_id in self.connections:
+            circle = c_circle.get_circle_whit_id(circle_id)
+            circle.connections.remove(self.id)
         
         c_circle.circles.pop(self.id)
     
@@ -40,6 +43,11 @@ class c_circle:
             if (c_circle.circles[i].x - pos[0]) ** 2 + (c_circle.circles[i].y - pos[1]) ** 2 <= 100:
                 return c_circle.circles[i]
         return None
+    
+    def get_circle_whit_id(id):
+        for i in c_circle.circles:
+            if c_circle.circles[i].id == id:
+                return c_circle.circles[i]
 
     @staticmethod
     def get_node_types():
@@ -55,8 +63,21 @@ class c_circle:
         ]
     
     @staticmethod
-    def new_circle(x, y, node_type, floor, cooridinates_absolute, name = "test"):
-        c_circle(x, y, node_type, floor, cooridinates_absolute, name)
+    def new_circle(x, y, node_type, floor, cooridinates_absolute, name = "test", connections = None, id = None):
+
+        connections = connections if connections != None else []
+        if id != None:
+            c_circle.next_id = id
+        c_circle.circles[c_circle.next_id] = c_circle(x, y, node_type, floor, cooridinates_absolute, name, connections)
         c_circle.next_id += 1
+        
+    @staticmethod
+    def load_file(path):
+        try:
+            with open('circles.json', 'r') as file:
+                circles = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            circles = {} 
 
-
+        for i in circles:
+            c_circle.new_circle(circles[i]["x"], circles[i]["y"], circles[i]["node_type"], circles[i]["floor"], (circles[i]["x"], circles[i]["y"]), circles[i]["name"], circles[i]["connections"], circles[i]["id"])
